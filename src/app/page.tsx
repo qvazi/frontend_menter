ed98ddb08916042919d7cec3b90616d45b67a55c
Aleksandr <qvazi-s@ya.ru>
2024-07-22 17:43:29 +0500

    import image main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 import Image from "next/image";
+import nextSvg from "../../public/next.svg";
 
 const Page = () => {
   return (
     <div>
       <h1 className="bg-red-400 text-red-200">Main Page</h1>
-      <Image
-        src={"/next.svg"}
-        alt="logo nextjs"
-        width={394}
-        height={80}
-        priority
-      />
+      <Image src={nextSvg} alt="logo nextjs" width={394} height={80} priority />
     </div>
   );
 };
