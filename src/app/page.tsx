6d945704f0ee40d8928a1626a598153171df7fe8
Aleksandr <qvazi-s@ya.ru>
2024-07-22 17:28:29 +0500

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+
 const Page = () => {
   return (
     <div>
       <h1 className="bg-red-400 text-red-200">Main Page</h1>
+      <Image
+        src={"/next.svg"}
+        alt="logo nextjs"
+        width={394}
+        height={80}
+        priority
+      />
     </div>
   );
 };
